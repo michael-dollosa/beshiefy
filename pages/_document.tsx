8cbd3dff98b9c262fa650f38f3d53113b5467ebc
Michael Dollosa <dollosa.christian@gmail.com>
2023-07-07 03:03:58 +0800

    fixed unstyled on mount:

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { createGetInitialProps } from '@mantine/next'
 
-export default function Document () {
-  return (
-    <Html lang='en'>
-      <Head>
-        <meta charSet='utf-8' />
-        <meta
-          name='viewport'
-          content='width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover'
-        />
-        <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta
-          name='apple-mobile-web-app-status-bar-style'
-          content='black-translucent'
-        />
-        <meta name='title' content='Beshiefy' />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const getInitialProps = createGetInitialProps()
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps
+
+  render () {
+    return (
+      <Html lang='en'>
+        <Head>
+          <meta charSet='utf-8' />
+          <meta
+            name='viewport'
+            content='width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover'
+          />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta
+            name='apple-mobile-web-app-status-bar-style'
+            content='black-translucent'
+          />
+          <meta name='title' content='Beshiefy' />
+          <script>0</script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
